77a28a05829bb40b079df906ba8dd5b9b07bff09
Hiimeka <116894382+Hiimeka@users.noreply.github.com>
2025-03-18 14:08:09 +0800

    config.js
    something update

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
     prefix: '!',
     nodes: [
     {
-        host: "node.optiklink.com",
-        password: "firmxnproject",
-        port: 5849,
+        host: "Your Host Lavalink",
+        password: "Your Password Lavalink",
+        port: Your Port Lavalink Host,
         secure: false,
         name: "Main Node"
     },
@@ -13,6 +13,6 @@ module.exports = {
         clientId: "a568b55af1d940aca52ea8fe02f0d93b",
         clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
     },
-    botToken: "MTM1MTAxNDQ4MTM1NDAzNTI3MA.GLNeZX.Bpam_KMqQOC6zrjs7-SQJFIt1_iqPyFWfW6fM0",
+    botToken: "Your Token",
     embedColor: "#0061ff"
 };
