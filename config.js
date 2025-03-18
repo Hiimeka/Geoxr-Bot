669dec8e2cac04fc777dc6ff777a1f67a1c322e8
Hiimeka <116894382+Hiimeka@users.noreply.github.com>
2025-03-18 14:06:11 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    prefix: '!',
+    nodes: [
+    {
+        host: "node.optiklink.com",
+        password: "firmxnproject",
+        port: 5849,
+        secure: false,
+        name: "Main Node"
+    },
+],
+    spotify: {
+        clientId: "a568b55af1d940aca52ea8fe02f0d93b",
+        clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
+    },
+    botToken: "MTM1MTAxNDQ4MTM1NDAzNTI3MA.GLNeZX.Bpam_KMqQOC6zrjs7-SQJFIt1_iqPyFWfW6fM0",
+    embedColor: "#0061ff"
+};
